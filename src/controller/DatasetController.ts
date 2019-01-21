19ccb5ed1fac8f1e02dba013086eb5439fe72684
Alexis Gervacio <lexigerv@gmail.com>
2019-01-21 01:36:00 -0800

    starting parse

--- src/controller/DatasetController.ts
+++ src/controller/DatasetController.ts
@@ -1,8 +1,21 @@
 import Log from "../Util";
+import * as JSzip from "jszip";
+import * as JSZip from "jszip";
+import {types} from "util";
+import {InsightDatasetKind} from "./IInsightFacade";
+// import isStringObject = module
+// import isNumberObject = module
+
 /**
  * Helper class to help parse and control datasets
  *
  */
+// BELOW IS FROM IINSIGHT
+// export interface InsightDataset {
+//     id: string;
+//     kind: InsightDatasetKind;
+//     numRows: number;
+// }
 export default class DatasetController {
     private data: Map<string, any[]>;
 
@@ -39,14 +52,63 @@ export default class DatasetController {
         return this.data.get(id);
     }
 
+    // ///////// load
+    // public loadData(file: string): Promise<any[]> {
+    //     return new Promise(async function (resolve, reject) {
+    //
+    //         let zip = new JSzip();
+    //         // let d: string[] = []; // TODO
+    //         zip.loadAsync(file, {base64: true});
+    //
+    //
+    //     });
+    //
+    //     try {
+    //         // let pArr: any[] = [];
+    //         // zip.loadAsync(file, {base64: true})
+    //
+    //
+    //     } catch (error) {
+    //
+    //
+    //     }
+    // }
+    //
+    // // public parseZip(zip: JSZip): Promise<any[]> {
+    // //     const files: Promise<any[]>[] = [];
+    // //
+    // //     // zip.forEach(((path: string, file: JSzip) => {
+    // //     //     if (file.dir == true) {return;}
+    // //     //
+    // //     //     let f: any[] = [];
+    // //     //     f = await JSON.parse(data).res
+    // //     // }))
+    // //
+    // //     const f = await JSON.parse(f).result.map(createCourseEntry)
+    // // }
+    //
+    //
+    //
+    // public makeCourseEntry(e: any): any {
+    //     e.filter((item: any) => item !== null);
+    //
+    //     // CHECK VALID TYPE
+    //     // if (!isStringObject(e.Subject) ||
+    //     //     (!isStringObject(e.Course) && !isNumberObject(e.Course))
+    //     return {
+    //         courses_dept: e.Subject,
+    //         courses_id: e.Course,
+    //         courses_avg: e.Avg,
+    //         courses_instructor: e.Professor,
+    //         courses_title: e.Title,
+    //         courses_pass: e.Pass,
+    //         courses_fail: e.Fail,
+    //         courses_audit: e.Audit,
+    //         courses_uuid: e.id,
+    //         courses_year: e.Year    // TODO
+    //     };
+    // }
+
     ///// TODO: Add cache stuff
 } //// TODO: parsing
 //
-// export const datasetTraits: {
-//     [dataset: string] {
-//         processZip: (zip:JSZip) => Promise<any[]>,
-//         keys: {
-//
-//         }
-// }
-// }
--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -16,15 +16,42 @@ export default class InsightFacade implements IInsightFacade {
         Log.trace("InsightFacadeImpl::init()");
         this.datasetController = new DatasetController();
     }
+    //
+    // public loadData(file: string): Promise<any[]> {
+    //     return new Promise(async function (resolve, reject) {
+    //
+    //         let zip = new JSZip();
+    //         // let d: string[] = []; // TODO
+    //         zip.loadAsync(file, {base64: true});
+    //
+    //
+    //     });
+    //
+    //     try {
+    //         // let pArr: any[] = [];
+    //         // zip.loadAsync(file, {base64: true})
+    //
+    //
+    //     } catch (error) {
+    //
+    //
+    //     }
+    // }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         // return Promise.reject("Not implemented.");
-        return new Promise(function (resolve, reject) {
+        // async function ()
+        return new Promise(async function (resolve, reject) {
             try {
                // if (content != null)
                //  new JSZip().loadAsync(content)
+                let zip = await new JSZip().loadAsync(content, {base64: true});
+                const result = readZip(zip);
+                this.datasetController.addDataset(id, result);
 
+                resolve();
             } catch (error) {
+                Log.error(error);
                 reject("stub");
             }
         });
@@ -44,4 +71,62 @@ export default class InsightFacade implements IInsightFacade {
 
     // ============
     // Helper Functions
+
+    ///////// load
+
+    // public parseZip(zip: JSZip): Promise<any[]> {
+    //     const files: Promise<any[]>[] = [];
+    //
+    //     // zip.forEach(((path: string, file: JSZip) => {
+    //     //     if (file.dir == true) {return;}
+    //     //
+    //     //     let f: any[] = [];
+    //     //     f = await JSON.parse(data).res
+    //     // }))
+    //
+    //     const f = await JSON.parse(f).result.map(createCourseEntry)
+    // }
+
+    private readZip(zip: JSZip): Promise<any[]> {
+        const files: Array<Promise<any[]>> = [];
+        // TODO
+        return Promise.all(files);
+    }
+
+    private makeCourseEntry(e: any): any {
+        e.filter((item: any) => item !== null);
+
+        // CHECK VALID TYPE
+        // if (!isStringObject(e.Subject) ||
+        //     (!isStringObject(e.Course) && !isNumberObject(e.Course))
+        return {
+            courses_dept: e.Subject,
+            courses_id: e.Course,
+            courses_avg: e.Avg,
+            courses_instructor: e.Professor,
+            courses_title: e.Title,
+            courses_pass: e.Pass,
+            courses_fail: e.Fail,
+            courses_audit: e.Audit,
+            courses_uuid: e.id,
+            courses_year: e.Year    // TODO
+        };
+    }
 }
+const courseKeys: {
+    // [dataset: string]: {
+        // processZip: (zip: JSZip) => Promise<any[]>,
+        // keys: {
+            courses_dept: "string",
+            courses_id: "string",
+            courses_avg: "number",
+            courses_instructor: "string",
+            courses_title: "string",
+            courses_pass: "number",
+            courses_fail: "number",
+            courses_audit: "number",
+            courses_uuid: "string",
+            courses_year: "number"
+        // }
+    // }
+};
